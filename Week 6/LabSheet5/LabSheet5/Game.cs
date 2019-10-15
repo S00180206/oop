2358a487a5490d9095ad5baf32cc45f3edad6de9
joanthanodonnell <47103253+S00180206@users.noreply.github.com>
2019-10-15 14:00:54 +0100

    Week 6 Labsheet5-part1

---
+++ Week 6/LabSheet5/LabSheet5/Game.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabSheet5
+{
+    public abstract  class Game
+    {
+        #region Properties
+
+        //public string Name{get; set;}
+
+        private readonly string _name;
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+        }
+
+        public Game(string name, decimal price, DataTime releasedate )
+        {
+            _name = name;
+            Price = price;
+            releasedate = releasedate;
+
+        }
+        public Game(string name, decimal price): this(name, price, DateTime.Now)
+        {
+
+        }
+
+        public Game() : this("", 0) { }
+
+        #endregion Constructors
+    }
+}
---
+++ Week 6/LabSheet5/LabSheet5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabSheet5
+{
+    class Program
+    {
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int DateTime { get; set; }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
