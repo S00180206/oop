f537036e2e3fb14e8584c4a9c52dc0718921fb8a
joanthanodonnell <47103253+S00180206@users.noreply.github.com>
2019-10-21 11:37:23 +0100

    Added player class with 3 properties

---
+++ Mock CA/Ex1/Ex1/Player.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex1
+{
+    public class Player
+    {
+        public int ID { get; set; }
+
+        public string PlayerName { get; set; }
+
+        public int score { get; set; }
+
+
+    }
+}
